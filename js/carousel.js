9f44097aecb6d938fb8a4aaf04600849e468ac17
ndass6 <ND@USComputers.net>
2017-05-23 22:02:42 -0700

    Added slick carousel with carousel nav

---
+++ js/carousel.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    // centerPadding: 25,
+    arrows: true,
+    fade: true,
+    asNavFor: '.carousel-nav'
+  });
+  $('.carousel-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.carousel',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 8000
+  });
+});
