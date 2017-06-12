e1dcdd5a55bf4c9810b5c0300c27d77a96507065
ndass6 <ND@USComputers.net>
2017-06-12 00:09:54 -0700

    Fixed scrolling

--- js/scroll.js
+++ js/scroll.js
@@ -1,12 +1,3 @@
-//jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 0) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
