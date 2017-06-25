bb74b29daaf893b34e16d7c05c5ccb6cfadbeac1
ndass6 <ND@USComputers.net>
2017-06-25 14:28:34 -0700

    Removed contact form javascript

--- js/main.js
+++ js/main.js
@@ -7,16 +7,6 @@ jQuery(function($) {
 		});
 	});
 
-	//Ajax contact
-	var form = $('.contact-form');
-	form.submit(function () {
-		$this = $(this);
-		$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
-	});
-
 	//scrollspy
 	$('[data-spy="scroll"]').each(function () {
 		var $spy = $(this).scrollspy('refresh')
