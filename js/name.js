aea40566694b5566891053c81b81cd8e06dc9097
ndass6 <ND@USComputers.net>
2017-11-28 01:29:31 -0500

    Revert "Updated name"
    This reverts commit 3d6a189861127965ea1d583e445dd7d0af3bc38a.

--- js/name.js
+++ js/name.js
@@ -1,33 +1,69 @@
-window.onload = function () {
-    var l = Snap('#logo');
+var logoEl = document.querySelector('.name-animation');
+var pathEls = document.querySelectorAll('.name-animation path');
+var gEls = document.querySelectorAll('.name-animation g');
+var dotEls = document.querySelectorAll('.name-animation .dot g');
+var innerWidth = window.innerWidth;
+var maxWidth = 1082;
+var logoScale = innerWidth >= maxWidth ? innerWidth / maxWidth : 1;
+var logoTimeline = anime.timeline();
 
-    setTimeout( function() {
-        var logoTitle = 'Nathan Dass';
-        var logoRandom = '';
-        var logoTitleContainer = l.text(0, '98%', '');
-        var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
-        logoTitleContainer.attr({
-            fontSize: 315,
-            fontFamily: 'Open Sans',
-            fontWeight: '600',
-            color: '#ffffff'
-        });
+logoEl.style.transform = 'translateY(50px) scale('+logoScale+')';
 
-        function generateRandomTitle(i, logoRandom) {
-            setTimeout( function() {
-                logoTitleContainer.attr({ text: logoRandom });
-            }, i*70 );
-        }
-
-        for( var i=0; i < logoTitle.length+1; i++ ) {
-            logoRandom = logoTitle.substr(0, i);
-            for( var j=i; j < logoTitle.length; j++ ) { 
-                logoRandom += possible.charAt(Math.floor(Math.random() * possible.length)); 
-            }
-            generateRandomTitle(i, logoRandom);
-            logoRandom = '';
-        }
-
-    }, 500 );
-
+for (var i = 0; i < pathEls.length; i++) {
+  var pathEl = pathEls[i];
+  pathEl.setAttribute('stroke-dashoffset', anime.setDashoffset(pathEl));
 }
+
+for (var i = 0; i < gEls.length; i++) {
+  var gEl = gEls[i];
+  gEl.setAttribute('stroke', window.getComputedStyle(document.body).getPropertyValue('--name-color'));
+}
+for (var i = 0; i < dotEls.length; i++) {
+  var dotEl = dotEls[i];
+  dotEl.setAttribute('fill', window.getComputedStyle(document.body).getPropertyValue('--name-color'));
+}
+
+logoTimeline
+  .add({
+  targets: '.dot-e',
+  translateX: [
+    { value: -900, duration: 820, delay: 200, easing: 'easeInQuart' }
+  ],
+  scale: [
+    { value: [0, 1], duration: 200, easing: 'easeOutBack' },
+    { value: 0, duration: 20, delay: 800, easing: 'easeInQuart' }
+  ],
+  offset: 0
+})
+  .add({
+  targets: '.fill.in.first',
+  strokeDashoffset: {
+    value: [anime.setDashoffset, 0],
+    duration: 600,
+    delay: function(el, i, t) { return 1000 + ( i * 150 ); },
+    easing: 'easeOutQuart'
+  },
+  stroke: {
+    value: ['#FFF', function(el) { return anime.getValue(el.parentNode, 'stroke') } ],
+    duration: 500,
+    delay: 500,
+    easing: 'easeInQuad'
+  },
+  offset: 0
+})
+  .add({
+  targets: '.fill.in.last',
+  strokeDashoffset: {
+    value: [anime.setDashoffset, 0],
+    duration: 600,
+    delay: function(el, i, t) { return 1000 + document.querySelectorAll('.fill.in.first').length * 150 + ( i * 300 ); },
+    easing: 'easeOutQuart'
+  },
+  stroke: {
+    value: ['#FFF', function(el) { return anime.getValue(el.parentNode, 'stroke') } ],
+    duration: 500,
+    delay: 500,
+    easing: 'easeInQuad'
+  },
+  offset: 0
+});
